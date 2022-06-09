a72c83620eb0cb44ce3f62db55f545f2ea22921a
Sheshie1225 <mlallen1225@outlook.com>
2022-06-08 19:28:25 -0500

    app attached

---
+++ climate_app.py
@@ -0,0 +1,24 @@
+#climate app
+
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#create database
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
